44806f780fc9352bed29a6e1f6a6065c02eb4319
sirjerky <brownad78@gmail.com>
2014-08-08 09:07:01 -0700

    Finish up mmm and change up README

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Mean is the average of all the numbers.
 ex. [1, 2, 3] the mean will be 2.
 
 Median is the middle number of the group.
-ex. [1, 2, 3] the median is 2. If we add another number: [1, 2, 3, 4] the median because the
+ex. [1, 2, 3] the median is 2. If we add another number: [1, 2, 3, 4] the median becomes the
 average of the two middle numbers: 2.5.
 
 Mode is the most common number in the group.
 ex. [1, 1, 2, 2, 2, 3, 3] the mode here is 2.
 
-This program will take an input of numbers from the command line and return the results of each method.
+This program will take an input of numbers from the command line and return the results of each method.s
--- meanmedmode.js
+++ meanmedmode.js
@@ -1,13 +1,17 @@
-function getMean(numbers){
+function getMean(entry){
+	//pop off the first two entries
+	var numbers = entry.slice(2);
+	console.log(numbers);
 	var sum = 0;
-	for(var i = 2; i < numbers.length; i++){
+	for(var i = 0; i < numbers.length; i++){
 		sum = sum += Number(numbers[i]);
 	}
-	return sum / (numbers.length - 2);
+	return sum / (numbers.length);
 }
 
-function getMedian(numbers){
+function getMedian(entry){
 	var median;
+	var numbers = entry.slice(2);
 	// first we sort the array
 	numbers.sort();
 	// next check for even or odd
@@ -23,9 +27,36 @@ function getMedian(numbers){
 	
 }
 
-function getMode(numbers){
-	
+function getMode(entry){
+	var numbers = entry.slice(2);
+	var results=[];
+
+	for(var i = 0; i < numbers.length; i++){
+		var el = Number(numbers[i]);
+		if(results[el]){
+			
+			results[el]+=1;
+		}else{
+			results[el]=1;
+		}
+	}
+	var modes=[];
+	var maxCount = 0;
+	console.log(results);
+	for(var i = 0; i < results.length; i++){
+		if(results[i] > maxCount){
+			modes=[];
+			modes.push(i);
+			maxCount = results[i];
+		}else if(results[i] === maxCount){
+			modes.push(i);
+		}
+	}
+	return(modes);
 }
+
 
 console.log(getMean(process.argv));
 console.log(getMedian(process.argv));
+console.log(getMode(process.argv));
+
